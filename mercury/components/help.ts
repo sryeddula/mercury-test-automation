7f5c30aae69795476609953bab2516e181a347b2
Sudhakar Reddy <sryeddula@gmail.com>
2017-09-05 16:01:12 +0100

    formatted the document

--- mercury/components/help.ts
+++ mercury/components/help.ts
@@ -1,20 +1,19 @@
-import { element, by, $,$$, ElementFinder,ElementArrayFinder } from 'protractor';
-export class HelpSection{
+import { element, by, $, $$, ElementFinder, ElementArrayFinder } from 'protractor';
+export class HelpSection {
     container: ElementFinder
     toggle: ElementFinder
     dots: ElementArrayFinder
     header: ElementFinder
     item: ElementArrayFinder
     previousPage: ElementFinder
-    nextPage:ElementFinder
-    constructor()
-    {
-        this.toggle=$('.toggle-help')
-        this.container=$('app-help')
-        this.dots=this.container.$$('.help-footer__nav__item')
-        this.header=this.container.$('.header')
-        this.item=this.container.$$('.help-item p')
-        this.previousPage=this.container.$('.previous-page')
-        this.previousPage=this.container.$('.next-page')
+    nextPage: ElementFinder
+    constructor() {
+        this.toggle = $('.toggle-help')
+        this.container = $('app-help')
+        this.dots = this.container.$$('.help-footer__nav__item')
+        this.header = this.container.$('.header')
+        this.item = this.container.$$('.help-item p')
+        this.previousPage = this.container.$('.previous-page')
+        this.previousPage = this.container.$('.next-page')
     }
 }
